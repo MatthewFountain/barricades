b4493b6833f08e1d09de242e162f672851e28c82
Matthew Fountain <matthewdfountain@gmail.com>
2014-09-15 13:11:49 -0700

    "Even numbers."

---
+++ test_even_numbers2.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class IsEvenTests(unittest.TestCase):
+    def testOne(self):
+        self.failUnless(IsEven(1))
+
+    def testTwo(self):
+        self.failIf(IsEven(2))
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
+
+
