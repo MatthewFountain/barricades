841f59b0b7b24716f11775ba8e9fe54b37ede595
Matthew Fountain <matthewdfountain@gmail.com>
2014-10-19 22:40:28 -0700

    Tip calculator v4 3rd try

--- CV_Page/HomeJS.js
+++ CV_Page/HomeJS.js
@@ -1,6 +1,70 @@
 $(document).ready(function() {
-    $(".blog-title").click(function () {
-        $(this).fadeOut()
+
+        $(".name-photo").show(500);
+        $("#Home_content").show(500);
+        $(".About_post").hide(500);
+        $("#Work_Samples_content").hide(500);
+        $("#Homing_In_content").hide(500);
+        $("#Contact_content").hide(500);
+
+    $("#Home_tab").click(function() {
+
+        $(".name-photo").show(500);
+        $("#Home_content").show(500);
+        $(".About_post").hide(500);
+        $("#Work_Samples_content").hide(500);
+        $("#Homing_In_content").hide(500);
+        $("#Contact_content").hide(500);
 
     });
+
+    $("#About_tab").click(function() {
+
+        $(".name-photo").show(500);
+        $("#Home_content").hide(500);
+        $(".About_post").show(500);
+        $("#Work_Samples_content").hide(500);
+        $("#Homing_In_content").hide(500);
+        $("#Contact_content").hide(500);
+
+    });
+
+     $("#Work_Samples_tab").click(function() {
+        $(".name-photo").show(500);
+        $("#Home_content").hide(500);
+        $(".About_post").hide(500);
+        $("#Work_Samples_content").show(500);
+        $("#Homing_In_content").hide(500);
+        $("#Contact_content").hide(500);
+
+    });
+
+    $("#Homing_In_tab").click(function() {
+
+        $(".name-photo").hide(500);
+        $("#Home_content").hide(500);
+        $(".About_post").hide(500);
+        $("#Work_Samples_content").hide(500);
+        $("#Homing_In_content").show(500);
+        $("#Contact_content").hide(500);
+
+    });
+
+    $("#Contact_tab").click(function() {
+        $(".name-photo").hide(500);
+        $("#Home_content").hide(500);
+        $(".About_post").hide(500);
+        $("#Work_Samples_content").hide(500);
+        $("#Homing_In_content").hide(500);
+        $("#Contact_content").show(500);
+
+    });
+
 });
+
+
+
+
+
+
+
--- MatthewFountain/HomeJS.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".blog-title").click(function () {
-        $(this).fadeOut()
-
-    });
-});
